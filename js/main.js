49d54c5c67bb1fb49b06f2df143e8dce08300350
mftrhu <mftrhu+github@inventati.org>
2016-11-14 18:39:26 +0100

    Making the fiddle/bin/testbed work with html + css

---
+++ js/main.js
@@ -0,0 +1,36 @@
+[
+  "html",
+  "js",
+  "css",
+  "result"
+].forEach(function (element, index, array) {
+  document
+  .getElementById("show-" + element)
+  .addEventListener("click", function (event) {
+    var button = document.getElementById("show-" + element);
+    var toggle = document.getElementById(element);
+    if (button.className === "unselected") {
+      button.className = "";
+      toggle.style.display = "";
+    } else {
+      button.className = "unselected";
+      toggle.style.display = "none";
+    }
+  });
+});
+
+document
+.getElementById("run")
+.addEventListener("click", function (event) {
+  var html = document.getElementById("html").value;
+  var css = document.getElementById("css").value;
+  var page = "data:text/html;charset=utf-8," +
+    encodeURIComponent(
+    "<!DOCTYPE html>"+
+    "<html lang=\"en\">"+
+    "<head><style>" + css + "</style></head>"+
+    "<body>" + html + "</body>"+
+    "</html>"
+  );
+  document.getElementById('result').setAttribute('src', page);
+});
